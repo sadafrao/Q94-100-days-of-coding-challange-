4f5643597091c707fd3d913ec9b088a3b5e2aa22
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:42:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Q94
+//make a function and return function use a method .map()using array 
+var color = ["Red", "green", "yellow"];
+var lengths = color.map(function (color) { return color.length; });
+console.log(lengths);
---
+++ index.ts
@@ -0,0 +1,5 @@
+// Q94
+//make a function and return function use a method .map()using array 
+let color : string[] = ["Red","green","yellow"];
+let lengths : number[] =color.map(color => color.length);
+console.log(lengths);
